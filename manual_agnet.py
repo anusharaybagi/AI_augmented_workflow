ac0893727419a4d80e3716b05537abcffa369bbd
Anusha Raybagi <anusharaybagi@gmail.com>
2026-08-12 11:32:03 +0530

    added manual study assistant

---
+++ manual_agnet.py
@@ -0,0 +1,24 @@
+*print("================================")
+print("       SIMPLE QUIZ")
+print("================================")
+
+answer = input("What is the capital of France? ")
+
+if answer.lower() == "paris":
+    print("Correct!")
+else:
+    print("Wrong answer.")
+
+answer = input("What is the capital of India? ")
+
+if answer.lower() == "delhi":
+    print("Correct!")
+else:
+    print("Wrong answer.")
+
+answer = input("Which language is commonly used in AI? ")
+
+if answer.lower() == "python":
+    print("Correct!")
+else:
+    print("Wrong answer.")*
